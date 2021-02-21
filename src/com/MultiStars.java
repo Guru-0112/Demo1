97082e556662e52995fce4a5b9154bc573c189b2
HOME <HOME@Raju>
2021-02-21 13:20:56 +0530

    changed hi

--- src/com/MultiStars.java
+++ src/com/MultiStars.java
@@ -5,7 +5,7 @@ public class MultiStars {
 	public static void main(String[] args) {
   for (int i = 1; i <=3; i++) {
 	  for (int j = 1; j <=i; j++) {
-		  System.out.println("*");
+		  System.out.println("hi");
 		
 	}
 	  System.out.println(" ");
